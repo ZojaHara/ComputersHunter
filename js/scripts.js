66476e10f5adf99dfca9b05c17f1f8d1e25f0adb
ZojaHara <k.harasimiuk@gmail.com>
2016-10-26 17:56:08 +0200

    select form added

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,64 @@
-// ;(function() {
-//
-//
-//
-// })();
+;(function() {
+
+		var form = document.querySelector("#myForm");
+		var select = document.querySelectorAll(".select");
+
+
+			var elems = [];
+			function selectValue() {
+				for(var i=0;i<select.length;i++) {
+					if(select[i].value==="") {
+						elems.push("hej");
+					}
+				}
+				console.log(elems);
+			}
+
+
+			function displayBox() {
+				selectValue();
+				if(elems.length===0) {
+					console.log("foto");
+				}else {
+					console.log("alert");
+				}
+
+			}
+
+
+
+		form.addEventListener("submit", function(e){
+
+			e.preventDefault();
+			displayBox();
+
+
+		}, false);
+
+
+})();
 
 
 (function($) {
+
+
+	$(document).ready(function() {
+
+		$('a').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+			if( target.length ) {
+				event.preventDefault();
+				$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 700);
+		}
+	});
+
+});
+
+
 
 	$(".menu__pic-hamburger").on("click", function() {
 
@@ -20,7 +73,13 @@
 
 			$(".menu__items").each(function() {
 				$(this).animate({
-					top:"0%"
+					top:0
+				},1000, "linear");
+			});
+
+			$(".menu__words-blue").each(function() {
+				$(this).animate({
+					fontSize:0
 				},1000, "linear");
 			});
 	});
@@ -43,6 +102,12 @@
 			$(".menu__items-right").animate({
 					top:"75%"
 			},1000, "linear");
+
+			$(".menu__words-blue").each(function() {
+				$(this).animate({
+					fontSize:"9vmin"
+				},1000, "linear");
+			});
 
 
 	});
